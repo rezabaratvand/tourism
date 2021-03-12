38f36eb25132af694f8e6d39ffdd6a8c9ece74c6
Reza Baratvand <rezabaratvand96@gmail.com>
2021-03-12 15:46:16 +0330

    Added tour controller

--- src/modules/tours/tours.controller.ts
+++ src/modules/tours/tours.controller.ts
@@ -1,34 +1,70 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { ToursService } from './tours.service';
 import { CreateTourDto } from './dto/create-tour.dto';
 import { UpdateTourDto } from './dto/update-tour.dto';
+import { MongoIdDto } from 'src/common/dto/mongoId.dto';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiCreatedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('tours')
 @Controller('tours')
 export class ToursController {
   constructor(private readonly toursService: ToursService) {}
 
   @Post()
-  create(@Body() createTourDto: CreateTourDto) {
-    return this.toursService.create(createTourDto);
+  @ApiOperation({ summary: 'create new tour' })
+  @ApiCreatedResponse()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createTourDto: CreateTourDto) {
+    return await this.toursService.create(createTourDto);
   }
 
   @Get()
-  findAll() {
-    return this.toursService.findAll();
+  @ApiOperation({ summary: 'get all tours' })
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  async findAll() {
+    return await this.toursService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.toursService.findOne(+id);
+  @ApiOperation({ summary: 'get a tour by id' })
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param() mongoIdDto: MongoIdDto) {
+    return await this.toursService.findOne(mongoIdDto);
   }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateTourDto: UpdateTourDto) {
-    return this.toursService.update(+id, updateTourDto);
+  @Patch(':id')
+  @ApiOperation({ summary: 'update a tour by id' })
+  @ApiCreatedResponse()
+  @HttpCode(HttpStatus.CREATED)
+  async update(
+    @Param() mongoIdDto: MongoIdDto,
+    @Body() updateTourDto: UpdateTourDto,
+  ) {
+    return await this.toursService.update(mongoIdDto, updateTourDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.toursService.remove(+id);
+  @ApiOperation({ summary: 'delete a tour by id' })
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  async remove(@Param() mongoIdDto: MongoIdDto) {
+    return await this.toursService.remove(mongoIdDto);
   }
 }
