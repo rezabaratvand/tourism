0a38ebdf06bc75ebcabc017d94ea1ba5f3ee9151
Reza Baratvand <rezabaratvand96@gmail.com>
2021-03-12 14:22:43 +0330

    Added swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './exception/exception-filter.exception';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { HttpExceptionFilter } from './exception/exception-filter.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // global exception filter
   // app.useGlobalFilters(new HttpExceptionFilter());
+
+  // swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Tourism')
+    .setDescription('The Tourism Api')
+    .setVersion('1.0')
+    .addTag('tourism')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
